2c51370caca96dad638bf8630299a4fdcea13fb8
onyx <onyx.pixart@gmail>
2022-06-05 18:50:21 +0200

    #2

--- controllers/controller.ControllerClass.php
+++ controllers/controller.ControllerClass.php
@@ -4,37 +4,32 @@ require_once __DIR__ . '/../models/interface/model.Interface.Model.php';
 require_once __DIR__ . '/../models/class/model.class.modelClassDataBase.php';
 require_once __DIR__ . '/../lib/debug/lib.debug.Display.php';
 
-class ControllerClass
+class ControllerModel
 {
     private $model = Model::MODEL_CLASS;
     private $method = false;
     private $field = false;
     private $value = false;
-    private $operator = false;
 
-    private $db_request = [];
-
-    public function Response()
+    public function Response($data)
     {
-
         $this->method = $_SERVER['REQUEST_METHOD'];
 
         if( $this->method == 'POST')
         {
-            $this->postResponse();
+            return $this->postResponse();
         }
         else if( $this->method == 'GET')
         {
-            //Display::print("get");
             return $this->getResponse();
         }
         else if( $this->method == 'PUT')
         {
-            $this->putResponse();
+            return $this->putResponse();
         }
         else if( $this->method == 'DELETE')
         {
-            $this->deleteResponse();
+            return $this->deleteResponse();
         }
         else
         {
@@ -48,7 +43,51 @@ class ControllerClass
 
     public function postResponse()
     {
-        Display::print("post request");
+        //need all field for insert
+        if(sizeof(ModelClass::CLASS_ARRAY) == sizeof($_POST))
+        {
+            $bool = true;
+            $post = $_POST;
+            $columns = array();
+            $fields = array();
+
+            //return array(ModelClass::CLASS_ARRAY, array_keys( $post ));
+
+            foreach($post as $key => $value)
+            {
+                //Display::print($value);
+                //return array($key, $value);
+                if(!in_array($key, ModelClass::CLASS_ARRAY))
+                {
+                    $bool = false;
+                }
+
+                $columns[] = $key;
+                $fields[] = $value;
+            }
+             // TO DO CHECK THE POST VALUE TO CONTINUTE
+            if($bool)
+            {
+
+                //return array($columns, $fields);
+                $res = DataBase::insert_fields($this->model, $columns, $fields);
+
+                if($res != false)
+                {
+                    return $res;
+                }
+                //return $post;
+                //return $this->successResponse("all field is good");
+            }
+        }
+
+        //return array(sizeof(ModelClass::CLASS_ARRAY), sizeof($_POST));
+        //return array( $_GET, $_POST);
+        
+        //$post = $_POST;
+
+        //return $post['NAME'];
+        return $this->errorResponse("all error");
     }
 
     public function getResponse()
@@ -60,7 +99,7 @@ class ControllerClass
         {
             //Display::print("test");
 
-            if(in_array($_GET['field'], ModelClass::CLASS_ARRAY))
+            if(in_array($_GET['field'], $this->getClass()))
             {
                 $this->field = $_GET['field'];
                 //Display::print( $this->successResponse("field found") );
@@ -86,12 +125,81 @@ class ControllerClass
 
     public function putResponse()
     {
-        Display::print("put request");
+        $get = $_GET;
+
+        if(isset($get['ID']))
+        {
+            //$id = intval( $get['ID'] );
+            $id = $_GET['ID'];
+
+            
+            if(is_int($id) || $id != 0)
+            {
+                //return array('ID found', $get);
+            
+                unset($get['model']);
+                if(!isset($get['model']))
+                {
+                    //return $this->successResponse("model destroy");
+                    unset($get['ID']);
+                    if(!isset($get['ID']))
+                    {
+                        //return array('all is destroy', $get);
+                        $bool = true;
+                        $columns = array();
+                        $fields = array();
+
+                        foreach($get as $key => $value)
+                        {
+                            //Display::print($value);
+                            //return array($key, $value);
+                            if(!in_array($key, $this->getClass()))
+                            {
+                                $bool = false;
+                            }
+            
+                            $columns[] = $key;
+                            $fields[] = $value;
+                        }
+                        
+                        if($bool)
+                        {
+                            $res = DataBase::edit_fields($this->model, $columns, $fields, $id);
+
+                            if($res)
+                            {
+                                return $res;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+ 
+
+        return $this->errorResponse("all error");
     }
 
     public function deleteResponse()
     {
-        Display::print("delete request");
+        $get = $_GET;
+
+        if(isset($get['ID']))
+        {
+            $id = $get['ID'];
+            if(is_int($id) || $id != 0)
+            {
+                $res = DataBase::delete_field($this->model, $id);
+
+                if($res != false)
+                {
+                    return $res;
+                }
+                return $res;
+                //return $this->successResponse("id found & compatible");
+            }
+        }
+        return $this->errorResponse("all error");
     }
 
     /**
@@ -106,6 +214,23 @@ class ControllerClass
     public function errorResponse($data): string
     {
         return "error with (" . $data . ")";
+    }
+
+    /**
+     * 
+     */
+
+    public static function getClass()
+    {
+        $array = ModelClass::CLASS_ARRAY;
+        array_unshift($array, Model::NAME_ID);
+
+        return $array;
+    }
+
+    public static function getPost($data)
+    {
+        return gettype($data);
     }
 
 }
