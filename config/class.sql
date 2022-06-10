65a8923371cd832104d79209c0c7b412b747959c
onyx <onyx.pixart@gmail>
2022-06-10 14:33:37 +0200

    #10

--- config/class.sql
+++ config/class.sql
@@ -2,6 +2,7 @@
 CREATE TABLE `class` (
   `ID` int(11) AUTO_INCREMENT PRIMARY KEY,
   `NAME` varchar(100) NOT NULL,
+  `NBR_CLASS` varchar(100) NOT NULL,
   `TIME` datetime NOT NULL,
   `DURATION` int(11) NOT NULL,
   `CURSUS` int(11) NOT NULL
