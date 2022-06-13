61e2defa2bcb0b5fb7d88891d89bf797b7c0c109
onyx <onyx.pixart@gmail>
2022-06-13 13:21:49 +0200

    #10

---
+++ config/profil_populate.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `profil` (`ID`, `NAME`, `BIRTH`, `EMAIL`, `PASSWORD`, `STAT`, `CURSUS`) VALUES
+(1, 'Arthur Rubiralta', '2001-06-05', 'arthur.rubiralta@ecole-89.com', 'azerty', 2000, 3000),
+(2, 'Fabien Faure', '2022-06-26', 'fabien.faure@ecole-89.com', 'qwerty', 2000, 4000),
+(3, 'Issam Hachem', '2022-06-26', 'issam.hachem@ecole-89.com', 'qwerty', 1000, 3000);
